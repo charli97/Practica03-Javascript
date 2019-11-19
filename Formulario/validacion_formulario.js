6d2f1f234cde3703da851a2bd6ae01be7af6af2f
charli97 <charlidaniel009@gmail.com>
2019-11-19 06:29:29 -0500

    Correo Commit

--- Formulario/validacion_formulario.js
+++ Formulario/validacion_formulario.js
@@ -1,63 +1,127 @@
 function validarCamposObligatorios() {
     var bandera = true
+    var bandera2 = true
+    var bandera3 = true
+    var bandera4 = true
+    var bandera5 = true
+    var bandera6 = true
     for (var i = 0; i < document.forms[0].elements.length; i++) {
         var elemento = document.forms[0].elements[i]
-        if (elemento.value == '' && elemento.type == 'text' || elemento.type == 'password') {
+        if (elemento.value == '' && elemento.type == 'text' || elemento.type == '' && elemento.type == 'password') {
             if (elemento.id == 'cedula') {
                 document.getElementById('mensajeCedula').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'nombres'){
+            } else if (elemento.id == 'nombres') {
                 document.getElementById('mensajeNombre').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'apellidos'){
+            } else if (elemento.id == 'apellidos') {
                 document.getElementById('mensajeApellido').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'direccion'){
+            } else if (elemento.id == 'direccion') {
                 document.getElementById('mensajeDireccion').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'telefono'){
+            } else if (elemento.id == 'telefono') {
                 document.getElementById('mensajeTelefono').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'fechaNacimiento'){
+            } else if (elemento.id == 'fechaNacimiento') {
                 document.getElementById('mensajeFecha').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'correo'){
+            } else if (elemento.id == 'correo') {
                 document.getElementById('mensajeCorreo').innerHTML = '<br>* Este campo esta vacio.'
-            }else if(elemento.id == 'contrasena'){
+            } else if (elemento.id == 'contrasena') {
                 document.getElementById('mensajeContra').innerHTML = '<br>* Este campo esta vacio.'
             }
-            elemento.style.border = '3px red solid'
-            elemento.className = 'error'
-            bandera = false
+            elemento.style.border = '3px red solid';
+            elemento.className = 'error';
+            bandera = false;
         }
-        if(elemento.id == 'cedula' && elemento.value != ''){
+        if (elemento.id == 'cedula' && elemento.value != '') {
             var ced = document.getElementById('cedula').value;
             var arrayCed = ced.split("");
-            var coeficientes = [2,1,2,1,2,1,2,1,2];
+            var coeficientes = [2, 1, 2, 1, 2, 1, 2, 1, 2];
             var resultado = 0;
             multi = new Array(8);
-            for(var j = 0; j < arrayCed.length-1; j++){
+            for (var j = 0; j < arrayCed.length - 1; j++) {
                 multi[j] = coeficientes[j] * arrayCed[j];
-                if (multi[j] >= 10){
+                if (multi[j] >= 10) {
                     multi[j] = multi[j] - 9;
                 }
                 resultado = resultado + multi[j];
             }
-            var aux = resultado - (resultado%10) +10;
+            var aux = resultado - (resultado % 10) + 10;
             var verificador = aux - resultado;
-            if(verificador == arrayCed[9]){
-                document.write("ES CORRECTA LA CEDULA");
+            if (verificador != arrayCed[9]) {
+                bandera2 = false;
+                elemento.style.border = '3px red solid';
+                elemento.className = 'error';
+            }
+        }
+        if (elemento.id == 'fechaNacimiento' && elemento.value != '') {
+            var fecha = document.getElementById('fechaNacimiento').value;
+            var arrayFecha = fecha.split('');
+            if (arrayFecha[2] != '/' && arrayFecha[5] != '/' || arrayFecha[0] == '/' && arrayFecha[1] == '/' || arrayFecha[3] == '/' && arrayFecha[4] == '/' || arrayFecha[6] == '/' && arrayFecha[7] == '/' && arrayFecha[8] == '/' && arrayFecha[9] == '/') {
+                bandera3 = false;
+                elemento.style.border = '3px red solid';
+                elemento.className = 'error';
+            }
+        }
+        if (elemento.id == 'correo' && elemento.value != '') {
+            var correo = document.getElementById('correo').value;
+            var arrayCorreo = correo.split('');
+            var verificando = '';
+            var resultado = '';
+            var aux = 0;
+            for (var i = 0; i < arrayCorreo.length; i++) {
+                if (arrayCorreo[i] == '@') {
+                    aux = i;
+                    console.log(aux)
+                }
+            }
+            console.log(aux)
+            if (aux != 0 && aux >= 3) {
+                console.log(aux)
+                for (var j = aux; j < arrayCorreo.length; j++) {
+                    resultado = resultado + arrayCorreo[j];
+                }
+                console.log(resultado);
+            }else {
+                bandera6 = false;
+                elemento.style.border = '3px red solid';
+                elemento.className = 'error';
+            }
+            if (resultado == '@est.ups.edu.ec' || resultado == '@ups.edu.ec') {
+                
             }else{
-                document.write("ES INCORRECTA LA CEDULA");
+                console.log(resultado);
+                bandera6 = false;
+                elemento.style.border = '3px red solid';
+                elemento.className = 'error';
             }
         }
     }
     if (!bandera) {
         alert('Error: Campos Vacios!!')
+    } else {
+        if (!bandera2) {
+            alert('Error: Cedula Incorrecta!!')
+            bandera = false;
+        } else {
+            if (!bandera3) {
+                alert('Error: Formato de Fecha Incorrecto!!')
+                bandera = false;
+            } else {
+                if (!bandera6) {
+                    alert('Correo Incorrecto!!')
+                    bandera = false;
+                }
+            }
+
+        }
     }
     return bandera
 }
+
 
 function validarLetras(elemento) {
     if (elemento.value.length > 0) {
         var miAscii = elemento.value.charCodeAt(elemento.value.length - 1)
         console.log(miAscii)
 
-        if (miAscii >= 97 && miAscii <= 122 || miAscii >= 65 && miAscii <= 90) {
+        if (miAscii >= 97 && miAscii <= 122 || miAscii >= 65 && miAscii <= 90 || miAscii == 32) {
             return true
         } else {
             elemento.value = elemento.value.substring(0, elemento.value.length - 1)
